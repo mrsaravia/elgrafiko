a9c82eca1ef2a475f2fcb2aef1e14073fa136a7e
msaravia <michael@elgrafiko.com>
2013-05-11 11:54:31 -0400

    Margin Auto FIXED
    I've figured out that the reason why the content wasn't centering on IE
    was because it was missing in the <HTML> tag <!DOCTYPE> info.

--- contactform.php
+++ contactform.php
@@ -58,7 +58,7 @@ $headers = 'From: '.$email."\r\n".
  
 <!-- include your own success html here -->
  
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
